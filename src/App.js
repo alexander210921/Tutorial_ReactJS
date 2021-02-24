619234ee09a4115ef00e2362e4c8fb82181487d5
Franklin Simalij <alexandersimalij12@gmail.com>
2021-02-24 11:52:16 -0600

    MutandoEstados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import ComponenteFuncional from './Components/ComponenteFuncional'
+import Element from './Components/Component'
 function App() {
   return (
    <>
-   {/* Agregamos las props comosi fuesen atributos, (deben de ser con el mismo nombre con el que declaramos desde el componentes) */}
-    <ComponenteFuncional titulo={'Titulo desde la prop'} precio ={12} />
-    <ComponenteFuncional titulo={'Titulo desde la prop'} precio ={3} />
+    <Element />
 
    </>
   );
